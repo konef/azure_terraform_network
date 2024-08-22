09e468e125f28e594648f3ea6c6df6e9f619db32
konef <40858004+konef@users.noreply.github.com>
2024-08-22 18:55:07 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 resource "azurerm_virtual_network" "example" {
   name                = var.vnet_name
   address_space       = var.address_space
