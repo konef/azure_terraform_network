4ffccc71e8303645d1b9dff82028f9e71f633a91
konef <40858004+konef@users.noreply.github.com>
2024-08-22 18:54:15 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,54 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.vnet_name
+  address_space       = var.address_space
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = var.subnet_address_prefixes
+}
+
+### --- Vars ---
+variable "resource_group_name" {
+  description = "Name of the Resource Group"
+}
+
+variable "location" {
+  description = "Azure region where the resources will be created"
+}
+
+variable "vnet_name" {
+  description = "Name of the Virtual Network"
+}
+
+variable "address_space" {
+  description = "Address space for the Virtual Network"
+  type        = list(string)
+}
+
+variable "subnet_address_prefixes" {
+  description = "Address prefixes for the Subnet"
+  type        = list(string)
+}
+
+### --- Outputs ---
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.example.location
+}
+
+output "subnet_id" {
+  value = azurerm_subnet.example.id
+}
